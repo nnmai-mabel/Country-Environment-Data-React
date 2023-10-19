5595e1e0234f39dd3b7506a9a5862c1500a2f2eb
mainguyen <nguny089@mymail.unisa.edu.au>
2023-10-19 21:39:05 +1030

    feat: move components folder under src and display sample region card

--- Assig2/a2reactapp/src/App.js
+++ Assig2/a2reactapp/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import RegionList from './components/RegionList';
 import './App.css';
 
 function App() {
@@ -6,6 +6,9 @@ function App() {
         <div className="App container">
             <div className="bg-light py-1 mb-2">
                 <h2 className="text-center">Example Application</h2>
+            </div>
+            <div className="row justify-content-center">
+                <RegionList />
             </div>
         </div>
     );
---
+++ Assig2/a2reactapp/src/components/RegionList.js
@@ -0,0 +1,16 @@
+﻿const RegionList = () => {
+
+    return (
+        <div className="card col-4 mb-2" style={{ width: 18 + 'em' }}>
+            <img className="card-img-top" src="..." alt={"Image of "} />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">card description</p>
+                <p className="card-text">another description</p>
+                {/*<Link to={`/Products/${props.itemId}?searchText=${props.searchText}`} className="btn btn-primary">View Detail</Link>*/}
+            </div>
+        </div>
+    )
+}
+
+export default RegionList
