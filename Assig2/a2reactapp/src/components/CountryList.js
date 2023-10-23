5882ddc85cf41bff6000059096ba12fea43df950
mainguyen <nguny089@mymail.unisa.edu.au>
2023-10-23 14:46:54 +1030

    feat: add search country query and return warning statements if no countries available

--- Assig2/a2reactapp/src/components/CountryList.js
+++ Assig2/a2reactapp/src/components/CountryList.js
@@ -6,6 +6,7 @@ const CountryList = ({ }) => {
     let params = useParams();
 
     const [countryData, setCountryData] = useState([]); // set initial countryData to empty array
+    const [query, setQuery] = useState('');
 
     //const [regionId, setRegionId] = useState(params.regionId ?? 0); // set regionId to the URL regionId parameter
 
@@ -17,14 +18,23 @@ const CountryList = ({ }) => {
     {/*data fetched is an object, need to access the key "countryList", which is an array of object, then map to array in return*/ }
     useEffect(() => {
         //setRegionId(regionId);
-        fetch(`http://localhost:5256/api/B_Countries/CountryList/${regionId}`)
+        fetch(`http://localhost:5256/api/B_Countries/CountryList/${regionId}?searchText=${query}`)
             .then(response => response.json())
             .then(data => setCountryData(data.countryList))
             //.then(data => console.log("FEtch"))
             .catch(err => {
                 console.log(err);
             });
-    }, [regionId]) // regionId is a reactive value and needs to change on a re-render -> put in dependency list
+    }, [regionId, query]) // regionId is a reactive value and needs to change on a re-render -> put in dependency list
+
+    //Create the searchQuery() function after the useEffect hook to capture the textbox text value then use it to update the query state
+    function searchQuery(evt) {
+        // const value = evt.target.value;
+        const value = document.querySelector('[name="searchText"]').value;
+        //alert('value: ' + value);
+        setQuery(value);
+    }
+
     return (
         <div className="row">
             <div className="bg-success py-1 mb-2">
@@ -33,21 +43,34 @@ const CountryList = ({ }) => {
             <div className="col-3 py-1 mb-2">
                 <Link to={`/Regions`} className="btn btn-warning">Regions</Link>
             </div>
-               
-            
+
+            <div className="col-3">
+                <input type="text" name="searchText" className="form-control" placeholder="Type your query" />
+            </div>
+            <div className="col text-left">
+                {/*Attach a ReactJS event to the button called “searchQuery” using the ReactJS syntax onClick={searchQuery}*/}
+                <button type="button" className="btn btn-primary" onClick={searchQuery}>Search</button>
+            </div>
+
             {/*Change object's key and value to array*/}
-            {countryData.map((obj) => (
-                <Country
-                    key={obj.countryId}
-                    countryId={obj.countryId}
-                    countryName={obj.countryName}
-                    iso3={obj.iso3 }
-                    imageUrl={obj.imageUrl}
-                    cityCount={obj.cityCount}
-                />
-            )
-            )
-            }
+
+            {countryData.length > 0 ? (
+                countryData.map((obj) => (
+                    <Country
+                        key={obj.countryId}
+                        countryId={obj.countryId}
+                        countryName={obj.countryName}
+                        iso3={obj.iso3}
+                        imageUrl={obj.imageUrl}
+                        cityCount={obj.cityCount}
+                    />
+                )
+                )
+            ) : (
+                <div className="bg-warning py-1 mb-2">
+                    <h2 className="text-center">No Countries available</h2>
+                </div>
+            )}
         </div>
     )
 }
