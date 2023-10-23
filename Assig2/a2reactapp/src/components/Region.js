906a08a8fb713caf5a095ba03d55adaadd5c6914
mainguyen <nguny089@mymail.unisa.edu.au>
2023-10-23 14:05:16 +1030

    feat: link region card (instead of just a button) to country list

--- Assig2/a2reactapp/src/components/Region.js
+++ Assig2/a2reactapp/src/components/Region.js
@@ -4,12 +4,13 @@ const Region = (props) => {
 
     return (
         <div className="card col-4 mb-5" style={{ width: 25 + 'em' }}>
-            <img className="card-img-top" src={props.imageUrl} alt={"Image of " + props.regionName} />
-            <div className="card-body">
-                <h5 className="card-title">{props.regionName}</h5>
-                <p className="card-text">Country Count: {props.countryCount }</p>
-                <Link to={`/Countries/${props.regionId}`} className="btn btn-primary">View Countries</Link>
-            </div>
+            <Link to={`/Countries/${props.regionId}`} className="">
+                <img className="card-img-top" src={props.imageUrl} alt={"Image of " + props.regionName} />
+                <div className="card-body">
+                    <h5 className="card-title">{props.regionName}</h5>
+                    <p className="card-text">Country Count: {props.countryCount}</p>
+                </div>
+            </Link>
         </div>
     )
 }
