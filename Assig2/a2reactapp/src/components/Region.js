20566ed275cea31396e4cfd3a76a5ad242879235
mainguyen <nguny089@mymail.unisa.edu.au>
2023-10-29 11:44:31 +1030

    feat: resolve warning

--- Assig2/a2reactapp/src/components/Region.js
+++ Assig2/a2reactapp/src/components/Region.js
@@ -1,17 +1,13 @@
 ﻿import { Link } from "react-router-dom";
 
 const Region = (props) => {
-    //let regionImage = ""
-    //props.imageUrl != null && props.imageUrl != "" ? regionImage = props.imageUrl : regionImage = "https://e0.pxfuel.com/wallpapers/1010/550/desktop-wallpaper-light-earth-planet-for-section-%D0%BA%D0%BE%D1%81%D0%BC%D0%BE%D1%81-beautiful-planet-earth.jpg"
-    //const regionData = { regionId: props.regionId, regionImageUrl: regionImage, regionName: props.regionName, countryCount: props.countryCount }
 
     return (
         <div className="card col-3 mb-5 justify-content-center" style={{ width: 25 + 'rem', margin: 10, padding: 0 }}>
 
-            {props.imageUrl != null && props.imageUrl != "" ?
+            {props.imageUrl !== null && props.imageUrl !== "" ?
                 (
                     <img className="card-img-top" src={props.imageUrl} alt={"Image of " + props.regionName} style={{ height: 16 + 'rem', width: 100 + "%", margin: 0 }} />
-
                 ) : (
                     <div>
                         <img className="card-img-top" alt={"Image of " + props.regionName} style={{ height: 16 + 'rem', width: 100 + "%", margin: 0 }} src="https://e0.pxfuel.com/wallpapers/1010/550/desktop-wallpaper-light-earth-planet-for-section-%D0%BA%D0%BE%D1%81%D0%BC%D0%BE%D1%81-beautiful-planet-earth.jpg" />
